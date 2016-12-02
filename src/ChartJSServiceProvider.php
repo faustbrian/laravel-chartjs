15e3b7fa8f6145207c200914abaf815781e33f24
Brian Faust <hello@brianfaust.de>
2016-12-02 20:41:28 +0200

    Rename Service Provider

--- src/ChartJSServiceProvider.php
+++ src/ChartJSServiceProvider.php
@@ -11,7 +11,9 @@
 
 namespace BrianFaust\ChartJS;
 
-class ServiceProvider extends \BrianFaust\ServiceProvider\ServiceProvider
+use BrianFaust\ServiceProvider\ServiceProvider;
+
+class ChartJSServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
@@ -19,6 +21,7 @@ class ServiceProvider extends \BrianFaust\ServiceProvider\ServiceProvider
     public function boot()
     {
         $this->publishViews();
+
         $this->loadViews();
     }
 
